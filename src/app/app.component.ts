b52bc834dc3279f22e72d0219f954ff89fced3a4
beastmodewatt <fictionfreeway@gmail.com>
2022-12-18 20:33:30 -0500

    complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+/*
+Title: app.component.ts
+Author: William Watlington
+Date: 17 December 2022
+Description: main app component for gpa app
+*/
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gpa-calculator-app1';
+}
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,21 @@
+/*
+Title: not-found.component.ts
+Author: William Watlington
+Date: 17 December 2022
+Description: not found component for gpa app
+*/
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+
+
+export class NotFoundComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
